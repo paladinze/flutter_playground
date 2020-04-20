54c4b35e686fcff0e72b601fea0c8020e69cb8e4
paladinze <paladinze@hotmail.com>
2020-04-21 00:56:18 +0800

    extract stateless widget from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+
+  var textList = ['Ze is super super smart!', 'Manny is even better than Ze!'];
+  var textIdx = 0;
+
+  void handleClick(int index) {
+    setState(() {
+      textIdx = index;
+    });
+  }
+
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello2!'));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("such is life"),
+        ),
+        body: Column(children: <Widget>[
+          Question(textList[textIdx]),
+          RaisedButton(
+            child: Text("click me!"),
+            onPressed: () => handleClick(1),
+          ),
+          RaisedButton(
+            child: Text("click me!"),
+            onPressed: () => handleClick(0),
+          )
+        ],)
+      ),
+    );
   }
 }
